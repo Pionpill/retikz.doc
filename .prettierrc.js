e6cb37289a0fc35c4b57592a00af043bcea684de
pionpill <673486387@qq.com>
2025-03-02 15:13:53 +0800

    :wrench: 添加项目格式化配置

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    editorconfig: true,
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+    trailingComma: 'es5',
+};
