88b2fc3efa20ab9a938301143695de11c01a89c5
pionpill <673486387@qq.com>
2025-04-13 23:13:33 +0800

    :construction: 支持本地编译 mdx 文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mdx()],
   base: '/retikz.doc/',
   resolve: {
     alias: {
